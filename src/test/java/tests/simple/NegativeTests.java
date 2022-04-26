e71891d23c3016910c6d61592938004b04388349
Zyryanovvm <chel.cska11@gmail.com>
2022-04-26 22:51:56 +0500

    Start project

---
+++ src/test/java/tests/simple/NegativeTests.java
@@ -0,0 +1,29 @@
+package tests.simple;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NegativeTests {
+
+    @Test
+    void test01() {
+        assertTrue(false);
+    }
+    @Test
+    void test02() {
+        assertTrue(false);
+    }
+    @Test
+    void test03() {
+        assertTrue(false);
+    }
+    @Test
+    void test04() {
+        assertTrue(false);
+    }
+    @Test
+    void test05() {
+        assertTrue(false);
+    }
+}
---
+++ src/test/java/tests/simple/SnippedTests.java
@@ -0,0 +1,34 @@
+package tests.simple;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SnippedTests {
+    @Test
+    @Disabled
+    void test01() {
+        assertTrue(false);
+    }
+    @Test
+    @Disabled
+    void test02() {
+        assertTrue(false);
+    }
+    @Test
+    @Disabled
+    void test03() {
+        assertTrue(false);
+    }
+    @Test
+    @Disabled
+    void test04() {
+        assertTrue(false);
+    }
+    @Test
+    @Disabled
+    void test05() {
+        assertTrue(false);
+    }
+}
